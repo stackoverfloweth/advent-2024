7fa7dd676411e92d9a7844badcae150678571853
Evan Sutherland <stackoverfloweth@gmail.com>
2024-12-09 22:28:29 -0600

    part 2

--- src/9/second.spec.ts
+++ src/9/second.spec.ts
@@ -2,18 +2,18 @@ import { expect, test } from 'vitest'
 import { solve } from './second'
 import { actualInput, exampleInput } from './input'
 
-test('given test input, always return X', () => {
+test('given test input, always return 2858', () => {
   const input = exampleInput
 
   const result = solve(input)
 
-  expect(result).toBe(input)
+  expect(result).toBe(2858)
 })
 
-test.skip('given actual input, always return Y', () => {
+test('given actual input, always return 6418529470362', () => {
   const input = actualInput
 
   const result = solve(input)
 
-  expect(result).toBe(input)
+  expect(result).toBe(6418529470362)
 })
--- src/9/second.ts
+++ src/9/second.ts
@@ -1,3 +1,101 @@
-export function solve(input: string): unknown {
-  return input
+export function solve(input: string): number {
+  const disk = readDisk(input.split('').map(Number))
+  const compacted = compactDisk(disk)
+
+  printDisk(compacted)
+  const checksum = calculateChecksum(compacted)
+
+  return checksum
+}
+
+type File = { id: number, size: number }
+type EmptySpace = { id: undefined, size: number }
+type Memory = File | EmptySpace
+
+function readDisk(input: number[]): Memory[] {
+  let fileId = 0
+
+  return input.reduce<Memory[]>((disk, value, index) => {
+    const isFile = index % 2 === 0
+
+    if (isFile) {
+      disk.push(createFile(fileId++, value))
+    } else {
+      disk.push(createEmptySpace(value))
+    }
+
+    return disk
+  }, [])
+}
+
+function compactDisk(disk: Memory[]): Memory[] {
+  const compacted = [...disk]
+  const maxFileId = Math.max(...compacted.filter(isFile).map((memory) => memory.id))
+
+  for (let fileId = maxFileId; fileId > 0; fileId--) {
+    const fileIndex = compacted.findIndex((memory) => memory.id === fileId)
+
+    if (fileIndex === -1) {
+      throw new Error('FileId Not Found!')
+    }
+
+    const file = compacted[fileIndex]
+    const availableSpaceIndex = compacted.findIndex((memory) => isEmptySpace(memory) && memory.size >= file.size)
+    if (availableSpaceIndex === -1 || availableSpaceIndex > fileIndex) {
+      continue
+    }
+
+    const availableSpace = compacted[availableSpaceIndex]
+    const remainingSpace = availableSpace.size > file.size ? createEmptySpace(availableSpace.size - file.size) : undefined
+
+    compacted.splice(fileIndex, 1, createEmptySpace(file.size))
+    compacted.splice(availableSpaceIndex, 1, ...[file, remainingSpace].filter((memory) => !!memory))
+  }
+
+  return compacted
+}
+
+function createFile(id: number, size: number): Memory {
+  return { id, size }
+}
+
+function isFile(memory: Memory): memory is File {
+  return memory.id !== undefined
+}
+
+function createEmptySpace(size: number): Memory {
+  return { id: undefined, size }
+}
+
+function isEmptySpace(memory: Memory): memory is EmptySpace {
+  return memory.id === undefined
+}
+
+function printDisk(disk: Memory[]): void {
+  console.log(disk.map((x) => {
+    const id = x.id ?? '.'
+    return String(id).repeat(x.size)
+  }).join(''))
+}
+
+function calculateChecksum(disk: Memory[]): number {
+  const split = splitDisk(disk)
+
+  console.log({ split })
+
+  return split.reduce<number>((sum, id, index) => {
+    if (id === undefined) {
+      return sum
+    }
+
+    return sum + id * index
+  }, 0)
+}
+
+function splitDisk(disk: Memory[]): (number | undefined)[] {
+  return disk.reduce<(number | undefined)[]>((split, memory) => {
+    split.push(...new Array(memory.size).fill(memory.id))
+
+    return split
+  }, [])
 }
